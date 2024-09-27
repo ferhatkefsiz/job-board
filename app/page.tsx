2318f9123dccd4e368433f350b4cacb9cbc0340c
ferhatkefsiz <ferhatkefsiz@yahoo.com>
2024-09-27 15:57:15 +0300

    feat: add Hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image"
+import Hero from "@/modules/Hero"
 
 export default function Home() {
-  return <div></div>
+  return (
+    <div className="container">
+      <Hero />
+    </div>
+  )
 }
