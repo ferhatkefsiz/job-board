bca5e0deebe7a835e717ca83e9fa6dc66f9926aa
ferhatkefsiz <ferhatkefsiz@yahoo.com>
2024-10-01 02:55:09 +0300

    feat: add job board & card

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,40 @@
 import Hero from "@/modules/Hero"
+import JobBoard from "@/modules/JobBoard"
 
-export default function Home() {
+export default async function Home() {
+  const latestJobs = [
+    {
+      _id: "1",
+      title: "Software Engineer",
+      orgId: "org123",
+      description: "We are looking for a software engineer to join our team.",
+      orgName: "Acme Inc",
+      remote: "Remote",
+      type: "Full",
+      salary: 100000,
+      country: "United States",
+      state: "California",
+      city: "San Francisco",
+      countryId: "US",
+      stateId: "CA",
+      cityId: "SF",
+      jobIcon: "https://via.placeholder.com/150",
+      contactPhoto: "https://via.placeholder.com/150",
+      contactName: "John Doe",
+      contactPhone: "123-456-7890",
+      contactEmail: "",
+      createdAt: "",
+      updatedAt: ""
+    }
+  ]
+
   return (
-    <div className="container">
-      <Hero />
-    </div>
+    <>
+      <div className="container">
+        <Hero />
+      </div>
+
+      <JobBoard header={"Jobs"} jobs={latestJobs} />
+    </>
   )
 }
