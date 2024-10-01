c71792f6eb7549956acca95b7c58bd6b1fc2e585
ferhatkefsiz <ferhatkefsiz@yahoo.com>
2024-10-01 03:06:14 +0300

    refactor: footer

--- modules/Footer.tsx
+++ modules/Footer.tsx
@@ -1,7 +1,7 @@
 export default async function Footer() {
   return (
-    <footer>
-      <p>Job Board &copy; 2024 - All rights reserved</p>
+    <footer className="mt-auto my-4 justify-end flex container">
+      <p className="text-xs text-gray-600">Job Board &copy; 2024 - All rights reserved</p>
     </footer>
   )
 }
