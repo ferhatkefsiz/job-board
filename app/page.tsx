fcb92663cd0002393ce2fa514f09436d35c885ce
ferhatkefsiz <ferhatkefsiz@yahoo.com>
2024-10-02 02:04:44 +0300

    refactor: Update Home component to fetch latest jobs with organization and user data

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,16 @@
+import mongoose from "mongoose"
+
+import { addOrgAndUserData, JobModel } from "@/models/Job"
+
 import Hero from "@/modules/Hero"
 import JobBoard from "@/modules/JobBoard"
 
+import { withAuth } from "@workos-inc/authkit-nextjs"
+
 export default async function Home() {
-  const latestJobs = [
-    {
-      _id: "1",
-      title: "Software Engineer",
-      orgId: "org123",
-      description: "We are looking for a software engineer to join our team.",
-      orgName: "Acme Inc",
-      remote: "Remote",
-      type: "Full",
-      salary: 100000,
-      country: "United States",
-      state: "California",
-      city: "San Francisco",
-      countryId: "US",
-      stateId: "CA",
-      cityId: "SF",
-      jobIcon: "https://via.placeholder.com/150",
-      contactPhoto: "https://via.placeholder.com/150",
-      contactName: "John Doe",
-      contactPhone: "123-456-7890",
-      contactEmail: "",
-      createdAt: "",
-      updatedAt: ""
-    }
-  ]
+  const { user } = await withAuth()
+  await mongoose.connect(process.env.MONGO_URI as string)
+  const latestJobs = await addOrgAndUserData(await JobModel.find({}, {}, { limit: 5, sort: "-createdAt" }), user)
 
   return (
     <>
